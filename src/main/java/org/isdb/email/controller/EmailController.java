5c0a0869a0d10e62b51de9500af0d9fd443dcbd7
Shamaratun <shamaratunmeme@gmail.com>
2025-04-09 13:31:41 +0600

    mail varification

---
+++ src/main/java/org/isdb/email/controller/EmailController.java
@@ -0,0 +1,31 @@
+package org.isdb.email.controller;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.isdb.email.model.EmailRequest;
+import org.isdb.email.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.mail.MessagingException;
+
+@RestController
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+
+	@PostMapping("/send-email")
+	public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+		try {
+			emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+			return ResponseEntity.ok("Email sent successfully");
+		} catch (MessagingException | IOException | GeneralSecurityException e) {
+			return ResponseEntity.internalServerError().body("Failed to send email: " + e.getMessage());
+		}
+	}
+}
