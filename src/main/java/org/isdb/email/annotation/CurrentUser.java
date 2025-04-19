486ae0034b743c3ff174028ce2e2b62f3c3d3a27
Shamaratun <shamaratunmeme@gmail.com>
2025-04-19 14:15:26 +0600

    user login

---
+++ src/main/java/org/isdb/email/annotation/CurrentUser.java
@@ -0,0 +1,12 @@
+package org.isdb.email.annotation;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@AuthenticationPrincipal
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUser {
+}
